505b2b47db1ea8152cabdeb3e5330bbb52dedacd
Bobby Naluz <bnaluz9@gmail.com>
2023-03-30 09:18:08 -0700

    deletehandler added for notes

--- src/components/Note.tsx
+++ src/components/Note.tsx
@@ -3,13 +3,19 @@ import React from "react";
 type NoteProps = {
   author: string;
   body: string;
+  onDelete: any;
+  id: string;
 };
 
-const Note = ({ author, body }: NoteProps) => {
+const Note = ({ author, body, onDelete, id }: NoteProps) => {
   return (
-    <li className="pt-2 mt-2 mx-2 rounded-lg  bg-blue-100 shadow-xl shadow-gray-300  ">
+    <li
+      id={id}
+      className="pt-2 mt-2 mx-2 rounded-lg  bg-blue-100 shadow-xl shadow-gray-300  "
+    >
       <p className="font-bold ml-2">{author}</p>
       <p className="text-left ml-2">{body}</p>
+      <button onClick={onDelete}>Delete</button>
     </li>
   );
 };
--- src/components/NoteList.tsx
+++ src/components/NoteList.tsx
@@ -5,16 +5,16 @@ import Note from "./Note";
 
 const NoteList = ({ newNote, hideModal }: any) => {
   const [notes, setNotes] = useState([]);
+  const [isLoading, setIsLoading] = useState(false);
 
   useEffect(() => {
     async function fetchNotes() {
+      setIsLoading(true);
       const res = await fetch(
         "https://notes-bbb9b-default-rtdb.firebaseio.com/notes.json"
       );
       const resData = await res.json();
-
       const loadedNotes = [];
-
       for (const key in resData) {
         loadedNotes.push({
           id: key,
@@ -22,7 +22,9 @@ const NoteList = ({ newNote, hideModal }: any) => {
           body: resData[key].body,
         });
       }
+      //@ts-ignore
       setNotes(loadedNotes);
+      setIsLoading(false);
     }
     fetchNotes();
   }, []);
@@ -40,14 +42,31 @@ const NoteList = ({ newNote, hideModal }: any) => {
       }
     );
     const data = await res.json();
+
+    // @ts-ignore
+    setNotes((existingNotes) => [noteData, ...existingNotes]);
     console.log(data);
-    //@ts-ignore
-    // setNotes((existingNotes) => [noteData, ...existingNotes]);
+    console.log(notes);
+  };
+
+  const deleteNoteHandler = async (id: string) => {
+    await fetch(
+      `https://notes-bbb9b-default-rtdb.firebaseio.com/notes/${id}.json`,
+      {
+        method: "DELETE",
+        headers: {
+          "Content-Type": "applications/json",
+        },
+      }
+    );
+
+    setNotes(notes.filter((note) => note.id !== id));
+    console.log(notes);
   };
 
   return (
     <div className="pt-24">
-      {newNote && (
+      {!isLoading && newNote && (
         <Modal onClickModal={hideModal}>
           <NewPost onCancel={hideModal} onAddNote={addNoteHandler} />
         </Modal>
@@ -56,12 +75,19 @@ const NoteList = ({ newNote, hideModal }: any) => {
       <ul className="max-w-[50rem] grid grid-cols-3 ">
         {notes.map((eachNote) => (
           <Note
+            onDelete={() => deleteNoteHandler(eachNote.id)}
             key={eachNote.body}
+            id={eachNote.id}
             author={eachNote.author}
             body={eachNote.body}
           />
         ))}
       </ul>
+      {isLoading && (
+        <div className="text-center text-black">
+          <p>Loading notes...</p>
+        </div>
+      )}
     </div>
   );
 };
